9fd1dc0a8af164ff33d8943943498d89bd1e52e2
Suyash-Gaikwad9 <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-07-25 13:51:06 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.61%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -3,13 +3,16 @@ public:
     int bitwiseComplement(int n) {
         
         
+        
+        
+        int m = n;
+        int mask = 0;
+        
         //edge case
         if(n==0){
             return 1;
         }
         
-        int m = n;
-        int mask = 0;
         while(m!=0){
             mask = (mask<<1) | 1;
             m = m>>1;
