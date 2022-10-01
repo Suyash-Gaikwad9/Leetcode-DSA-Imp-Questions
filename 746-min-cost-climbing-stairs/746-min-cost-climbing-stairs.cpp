7b9c6bd62657c26b43d312affd272b9688a433a3
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-10-01 18:17:00 +0530

    Time: 11 ms (60.24%), Space: 13.7 MB (81.26%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -51,6 +51,23 @@ public:
         return min(dp[n-1], dp[n-2]);
     }
     
+    
+    int solve4(vector<int>& cost, int n){
+        
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+        
+        
+        for(int i = 2; i<n; i++){
+            int curr = cost[i] + min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return min(prev1, prev2);
+    }
+    
+    
     int minCostClimbingStairs(vector<int>& cost) {
         // int n = cost.size();
         // //this can be simplified
@@ -65,8 +82,11 @@ public:
         // return ans;
         
         
+        // int n = cost.size();
+        // return solve3(cost, n);
+        
         int n = cost.size();
-        return solve3(cost, n);
+        return solve4(cost,n);
         
     }
 };
