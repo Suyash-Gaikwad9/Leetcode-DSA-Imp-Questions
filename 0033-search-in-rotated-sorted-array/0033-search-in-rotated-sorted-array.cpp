62f824441c952bb60b3a59d5d6f2876a672bdb4b
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-10-10 22:32:01 +0530

    Time: 11 ms (27.42%), Space: 11.2 MB (28.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,39 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size()-1;
-        int mid;
-        while(start <= end){
-            mid = (start+end)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            if(nums[start] <= nums[mid]){
-                
-                if(target <= nums[mid] && target >= nums[start]){
-                    end = mid;
+class Solution
+{
+    public:
+        int search(vector<int> &nums, int target)
+        {
+            int start = 0;
+            int end = nums.size() - 1;
+            int mid;
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
                 }
-                else{
+                if (nums[start] <= nums[mid])
+                {
+
+                    if (target <= nums[mid] && target >= nums[start])
+                    {
+                        end = mid;
+                    }
+                    else
+                    {
+                        start = mid + 1;
+                    }
+                }
+                else if (target >= nums[mid] && target <= nums[end])
+                {
                     start = mid + 1;
                 }
-                
+                else
+                {
+                    end = mid;
+                }
             }
-            else if(target >= nums[mid] && target <= nums[end]){
-                start = mid+1;
-            }
-            else{
-                end = mid;
-            }
+            return -1;
         }
-        return -1;
-    }
 };
