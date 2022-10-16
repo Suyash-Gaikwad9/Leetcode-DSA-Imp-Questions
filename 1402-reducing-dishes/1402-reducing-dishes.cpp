47e0c49b941d8172acbed39b8fe873e35d1c65b9
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-10-16 20:11:29 +0530

    Time: 52 ms (41.61%), Space: 21 MB (20.61%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -50,6 +50,28 @@ public:
     }
     
     
+    int solveSO(vector<int>& satisfaction){
+        int n = satisfaction.size();
+        vector<vector<int> >dp(n+1, vector<int>(n+1, 0));
+        
+        vector<int>curr(n+1, 0);
+        vector<int>next(n+1, 0);
+        
+        
+        for(int index = n-1; index >= 0; index--){
+            for(int time = index; time >= 0; time--){
+                
+                int include = satisfaction[index] * (time + 1) + next[time+1];
+                int exclude = 0 + next[time];
+                curr[time] = max(include, exclude);
+                
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+    
+    
     
     int maxSatisfaction(vector<int>& satisfaction) {
         // //sort the vector
@@ -61,9 +83,14 @@ public:
         // vector<vector<int> >dp(n+1, vector<int>(n+1, -1));
         // return solveMem(satisfaction, 0, 0, dp);
         
+        // sort(satisfaction.begin(), satisfaction.end());
+        // int n = satisfaction.size();
+        // return solveTab(satisfaction);
+        
         sort(satisfaction.begin(), satisfaction.end());
         int n = satisfaction.size();
-        return solveTab(satisfaction);
+        return solveSO(satisfaction);
+        
         
         
     }
