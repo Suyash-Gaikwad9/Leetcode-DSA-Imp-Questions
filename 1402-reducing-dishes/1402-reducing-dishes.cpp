e6d732b0cf1846f37288390c24e1b3f288c66507
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-10-16 19:58:06 +0530

    Time: 74 ms (26.72%), Space: 20.8 MB (38.60%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int solve(vector<int>& satisfaction, int index, int time){
         //base case
-        if(index == satisfaction.size())
+        int n = satisfaction.size();
+        if(index == n)
             return 0;
         
         int include = satisfaction[index] * (time + 1) + solve(satisfaction, index+1, time+1);
@@ -14,7 +15,8 @@ public:
     
     int solveMem(vector<int>& satisfaction, int index, int time, vector<vector<int> >&dp){
         //base case
-        if(index == satisfaction.size())
+        int n = satisfaction.size();
+        if(index == n)
             return 0;
         
         if(dp[index][time] != -1){
@@ -30,16 +32,38 @@ public:
     }
     
     
+    int solveTab(vector<int>& satisfaction){
+        int n = satisfaction.size();
+        vector<vector<int> >dp(n+1, vector<int>(n+1, 0));
+        
+        
+        for(int index = n-1; index >= 0; index--){
+            for(int time = index; time >= 0; time--){
+                
+                int include = satisfaction[index] * (time + 1) + dp[index+1][time+1];
+                int exclude = 0 + dp[index+1][time];
+                dp[index][time] = max(include, exclude);
+                
+            }
+        }
+        return dp[0][0];
+    }
+    
+    
+    
     int maxSatisfaction(vector<int>& satisfaction) {
         // //sort the vector
         // sort(satisfaction.begin(), satisfaction.end());
         // return solve(satisfaction, 0, 0);
         
+        // sort(satisfaction.begin(), satisfaction.end());
+        // int n = satisfaction.size();
+        // vector<vector<int> >dp(n+1, vector<int>(n+1, -1));
+        // return solveMem(satisfaction, 0, 0, dp);
+        
         sort(satisfaction.begin(), satisfaction.end());
         int n = satisfaction.size();
-        vector<vector<int> >dp(n+1, vector<int>(n+1, -1));
-        return solveMem(satisfaction, 0, 0, dp);
-        
+        return solveTab(satisfaction);
         
         
     }
