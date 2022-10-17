73d62ac1e180a06d8f114f71386a0331d858cead
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-10-17 18:27:51 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -73,7 +73,7 @@ class Solution
         return dp[0][0];
     }
     
-    int solveSO(int n, int a[]){
+    int solveSO(int n, int *a){
         
         vector<int>currRow(n+1, 0);
         vector<int>nextRow(n+1, 0);
