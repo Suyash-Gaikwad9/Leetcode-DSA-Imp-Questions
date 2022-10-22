71986f598b585841c4914633acea922c8c7d5ed3
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-10-22 15:42:54 +0530

    Time: 93 ms (59.10%), Space: 28 MB (7.56%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     bool solve(string& str, string& pattern, int i, int j, vector<vector<int>>& dp){
         //base case
-        if(i<0 && j<0){
+        if(i==0 && j==0){
             return true;
         }
-        if(i >= 0 && j < 0){
+        if(i > 0 && j == 0){
             return false;
         }
-        if(i < 0 && j >= 0){
-            for(int k = 0; k <= j; k++){
-                if(pattern[k] != '*'){
+        if(i == 0 && j > 0){
+            for(int k = 1; k <= j; k++){
+                if(pattern[k-1] != '*'){
                     return false;
                 }
             }
             return true;
         }
         
-        
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
-        
         //match
-        if(str[i] == pattern[j] || pattern[j] == '?'){
+        if(str[i-1] == pattern[j-1] || pattern[j-1] == '?'){
             return dp[i][j] = solve(str, pattern, i-1, j-1, dp);
         }
-        else if(pattern[j] == '*'){
+        else if(pattern[j-1] == '*'){
             return dp[i][j] = (solve(str, pattern, i-1, j, dp) || solve(str, pattern, i, j-1, dp));
         }
         else{
@@ -37,8 +35,10 @@ public:
     
     
     
+    
+    
     bool isMatch(string s, string p) {
-        vector<vector<int>>dp(s.length(), vector<int>(p.length(), -1));
-        return solve(s, p, s.length()-1, p.length()-1, dp);
+        vector<vector<int>>dp(s.length()+1, vector<int>(p.length()+1, -1));
+        return solve(s, p, s.length(), p.length(), dp);
     }
 };
