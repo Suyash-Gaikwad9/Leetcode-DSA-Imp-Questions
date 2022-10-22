81d33e5c90e445c2a6d2b096daf0643ceef92eac
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-10-22 16:03:31 +0530

    Time: 50 ms (84.84%), Space: 7.1 MB (85.96%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -35,9 +35,12 @@ public:
     
     
     bool solveTab(string str, string pattern){
-        vector<vector<int>>dp(str.length()+1, vector<int>(pattern.length()+1, 0));
+        //vector<vector<int>>dp(str.length()+1, vector<int>(pattern.length()+1, 0));
         
-        dp[0][0] = true;
+        vector<int>prev(pattern.length()+1, 0);
+        vector<int>curr(pattern.length()+1, 0);
+        
+        prev[0] = true;
         for(int j = 1; j<=pattern.length(); j++){
             bool flag = true;
             for(int k = 1; k <= j; k++){
@@ -46,24 +49,25 @@ public:
                     break;
                  }
             }
-            dp[0][j] = flag;
+            prev[j] = flag;
         }
         
         for(int i = 1; i <= str.length(); i++){
             for(int j = 1; j <= pattern.length(); j++){
                 
                 if(str[i-1] == pattern[j-1] || pattern[j-1] == '?')
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 
                 else if(pattern[j-1] == '*')
-                    dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
+                    curr[j] = (prev[j] || curr[j-1]);
                 
                 else
-                    dp[i][j] = false;
+                    curr[j] = false;
             
         }
+            prev = curr;
     }
-        return dp[str.length()][pattern.length()];
+        return prev[pattern.length()];
 }
     
     
