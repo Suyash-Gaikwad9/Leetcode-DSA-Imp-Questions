00baa725ce5e0cd56f5dfdf02faf5cace090da2e
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-10-22 13:15:49 +0530

    Time: 15 ms (79.38%), Space: 6.3 MB (97.64%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -36,41 +36,48 @@ public:
     
     
     int solveTab(string &a, string &b){
-        vector<vector<int>>dp(a.length()+1, vector<int>(b.length()+1, 0));
+        //vector<vector<int>>dp(a.length()+1, vector<int>(b.length()+1, 0));
+        
+        
+        vector<int>curr(b.length()+1, 0);
+        vector<int>next(b.length()+1, 0);
+        
         
         for(int j = 0; j < b.length(); j++){
-            dp[a.length()][j] = b.length() - j;
+            next[j] = b.length() - j;
         }
-        
-        for(int i = 0; i < a.length(); i++){
-            dp[i][b.length()] = a.length() - i;
-        }
+
         
         for(int i = a.length()-1; i >= 0; i--){
             for(int j = b.length()-1; j >= 0; j--){
                 
+                //catch here - from base case
+                curr[b.length()] = a.length() - i;
+                
+                
                 int ans = 0;
                 if(a[i] == b[j]){
-                    ans = dp[i+1][j+1];
+                    ans = next[j+1];
                 }
                 else{
                     //insert
-                    int insertAns = 1 + dp[i][j+1];
+                    int insertAns = 1 + curr[j+1];
 
                     //delete
-                    int deleteAns = 1 + dp[i+1][j];
+                    int deleteAns = 1 + next[j];
 
                     //replace
-                    int replaceAns = 1 + dp[i+1][j+1];
+                    int replaceAns = 1 + next[j+1];
 
                     ans = min(insertAns, min(deleteAns, replaceAns));
 
                 }
-                dp[i][j] = ans;
+                curr[j] = ans;
                 
             }
+            next = curr;
         }
-        return dp[0][0];
+        return next[0];
     }
     
     
@@ -78,7 +85,12 @@ public:
     int minDistance(string word1, string word2) {
         // vector<vector<int>>dp(word1.length(), vector<int>(word2.length(), -1));
         // return solve(word1, word2, 0, 0, dp);
-        
+        if(word1.length() == 0){
+            return word2.length();
+        }
+        if(word2.length() == 0){
+            return word1.length();
+        }
         return solveTab(word1, word2);
     }
 };
