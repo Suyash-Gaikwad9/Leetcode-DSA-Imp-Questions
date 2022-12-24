77a5c334257ea57ec040a41be4472bf7148ef649
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2022-12-24 17:05:43 +0530

    Time: 3 ms (55.84%), Space: 6.2 MB (65.54%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long mod = 1e9+7;
+    int numTilings(int n) {
+        vector<int>dp(1001, -1);
+        dp[0] = 1, dp[1] = 1, dp[2] = 2;
+        for(int i = 3; i<=n; i++){
+            dp[i] = (2*dp[i-1]%mod + dp[i-3]%mod)%mod;
+        }
+        return dp[n];
+    }
+};
