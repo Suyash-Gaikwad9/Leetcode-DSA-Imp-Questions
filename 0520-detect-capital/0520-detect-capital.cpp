cac231950b4d02b17d7dcafd3a444718baf3faf1
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2023-01-02 22:43:58 +0530

    Time: 0 ms (100.00%), Space: 6 MB (89.31%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        bool allUpper = true, allLower = true;
+        if(word.size() == 0 || word.size() == 1){
+            return true;
+        }
+        for(int i = 1; i<word.size(); i++){
+            if(islower(word[i])){
+                allUpper = false;
+            }
+            if(isupper(word[i])){
+                allLower = false;
+            }
+        }
+        return allLower || (allUpper && isupper(word[0]));
+    }
+};
