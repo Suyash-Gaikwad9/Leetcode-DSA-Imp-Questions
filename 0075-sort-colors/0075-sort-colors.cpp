01a79ff4587e71408d7fe867305d5f653769389e
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2023-02-27 11:33:46 +0530

    Time: 3 ms (55.44%), Space: 8.3 MB (30.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,29 +4,22 @@ public:
         int low = 0;
         int mid = 0;
         int high = nums.size()-1;
-        int temp;
+        
         while(mid <= high){
             switch(nums[mid]){
-                case 0: {
-                    temp = nums[low];
-                    nums[low] = nums[mid];
-                    nums[mid] = temp;
-                    low++;
+                case 0:
+                    swap(nums[low], nums[mid]);
+                    low++, mid++;
+                    break;
+                case 1:
                     mid++;
                     break;
-                }
-                case 1: {
-                    mid++;
-                    break;
-                }
-                case 2:{
-                    temp = nums[mid];
-                    nums[mid] = nums[high];
-                    nums[high] = temp;
+                case 2:
+                    swap(nums[mid], nums[high]);
                     high--;
                     break;
-                }
             }
+            
         }
     }
 };
