edd59a641e47a38da6096e13449403d8d6494436
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2023-03-13 08:14:07 +0530

    Time: 7 ms (53.98%), Space: 16.5 MB (58.96%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,32 +1,24 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        bool isSymmetricHelp(TreeNode *left, TreeNode *right)
-        {
-            if (left == NULL || right == NULL)
-            {
-                return left == right;
-            }
-            if (left->val != right->val)
-            {
-                return false;
-            }
-            return isSymmetricHelp(left->left, right->right) &&
-                isSymmetricHelp(left->right, right->left);
-        }
-    bool isSymmetric(TreeNode *root)
-    {
-        return root == NULL || isSymmetricHelp(root->left, root->right);
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return root == NULL || isSymetricHelp(root->left, root->right);
+    }
+    bool isSymetricHelp(TreeNode* left, TreeNode* right){
+        if(left == NULL || right == NULL)
+            return left == right;
+        if(left->val != right->val)
+            return false;
+        return isSymetricHelp(left->left, right->right) && isSymetricHelp(left->right, right->left);
     }
 };
